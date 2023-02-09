4ce21038fcc9caf762727580487dda38e8cc7dac
Hyeonbin Seo <seohyunbin1@naver.com>
2023-02-09 15:37:29 +0900

    testcommit

---
+++ src/main/java/smallstudy/blogstudy/BlogstudyApplication.java
@@ -0,0 +1,13 @@
+package smallstudy.blogstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogstudyApplication.class, args);
+	}
+
+}
